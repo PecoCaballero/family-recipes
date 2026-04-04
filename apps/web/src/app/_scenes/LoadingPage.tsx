af9f6665c9d9dcbea5c798149a62a4b71e6191e0
Pedro Augusto de Almeida Caballero <peco.almeida.caballero@gmail.com>
2026-04-04 17:51:27 -0300

    feat: add translations to loading page

--- apps/web/src/app/_scenes/LoadingPage.tsx
+++ apps/web/src/app/_scenes/LoadingPage.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { CenteredFullPage } from '../_components/SceneComponents';
 import { CircularProgress } from '@mui/material';
 
 export function LoadingPage() {
+  const { t } = useTranslation();
+
   return (
     <CenteredFullPage sx={{ alignItems: 'center' }}>
       <CircularProgress />
-      <Typography>Loading...</Typography>
+      <Typography>{t('common.loading')}</Typography>
     </CenteredFullPage>
   );
 }
